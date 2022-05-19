ace1b0812afc84acd070809c6d65a8f4c46e0487
Adeolu <osunsamiadeolu@icloud.com>
2022-05-19 04:24:56 -0700

    task4: added service

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\User\UserCollection;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Get Users
+     * @authenticated
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+        // return (new UserCollection());
+    }
 }
---
+++ app/Services/UserService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+final class UserService
+{
+    /**
+     * Get all users
+     * @param array $filters
+     */
+    public function get_users(array $filters)
+    {
+        //
+    }
+}
